50611bd5bbea89c12763204cfd061a3c76450e66
delude88 <tobias.hegemann@googlemail.com>
2021-03-11 14:06:36 +0100

    Added async methods to auth service

--- src/ds_auth_service.cc
+++ src/ds_auth_service.cc
@@ -5,22 +5,24 @@
 #include <cpprest/http_client.h>
 #include <cpprest/json.h>
 #include <cpprest/uri.h>
+#include <iostream>
+#include <stdexcept>
 
 using namespace web;
 using namespace web::http;
 using namespace web::http::client;
 
-bool ds::ds_auth_service_t::verifyToken(const std::string& token)
+pplx::task<bool> ds::ds_auth_service_t::verifyToken(const std::string& token)
 {
   const std::string url = this->url;
-  auto postJson =
-      pplx::create_task([url, token]() {
-        http_client client(U(url + "/profile"));
-        http_request request(methods::POST);
-        request.headers().add(U("Content-Type"), U("application/json"));
-        request.headers().add(U("Authorization"), U("Bearer " + token));
-        return client.request(request);
-      }).then([](http_response response) {
+  return pplx::create_task([url, token]() {
+           http_client client(U(url + "/profile"));
+           http_request request(methods::POST);
+           request.headers().add(U("Content-Type"), U("application/json"));
+           request.headers().add(U("Authorization"), U("Bearer " + token));
+           return client.request(request);
+         })
+      .then([](http_response response) {
         // Check the status code.
         if(response.status_code() != 200) {
           return false;
@@ -28,9 +30,19 @@ bool ds::ds_auth_service_t::verifyToken(const std::string& token)
         // Convert the response body to JSON object.
         return true;
       });
+}
 
-  postJson.wait();
-  return postJson.get();
+bool ds::ds_auth_service_t::verifyTokenSync(const std::string& token)
+{
+  auto postJson = this->verifyToken(token);
+  try {
+    postJson.wait();
+    return postJson.get();
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return false;
+  }
 }
 
 ds::ds_auth_service_t::ds_auth_service_t(const std::string& authUrl)
@@ -38,54 +50,58 @@ ds::ds_auth_service_t::ds_auth_service_t(const std::string& authUrl)
   this->url = authUrl;
 }
 
-std::string ds::ds_auth_service_t::signIn(const std::string& email,
-                                          const std::string& password)
+pplx::task<std::string>
+ds::ds_auth_service_t::signIn(const std::string& email,
+                              const std::string& password)
 {
   const std::string url = this->url;
-  auto postJson =
-      pplx::create_task([url, email, password]() {
-        json::value jsonObject;
-        jsonObject[U("email")] = json::value::string(U(email));
-        jsonObject[U("password")] = json::value::string(U(password));
+  return pplx::create_task([url, email, password]() {
+           json::value jsonObject;
+           jsonObject[U("email")] = json::value::string(U(email));
+           jsonObject[U("password")] = json::value::string(U(password));
 
-        return http_client(U(url)).request(
-            methods::POST, uri_builder(U("login")).to_string(),
-            jsonObject.serialize(), U("application/json"));
+           return http_client(U(url)).request(
+               methods::POST, uri_builder(U("login")).to_string(),
+               jsonObject.serialize(), U("application/json"));
+         })
+      .then([](http_response response) {
+        // Check the status code.
+        if(response.status_code() != 200) {
+          throw std::invalid_argument("Returned " +
+                                      std::to_string(response.status_code()));
+        }
+        // Convert the response body to JSON object.
+        return response.extract_json();
       })
-          .then([](http_response response) {
-            // Check the status code.
-            if(response.status_code() != 200) {
-              throw std::invalid_argument(
-                  "Returned " + std::to_string(response.status_code()));
-            }
-            // Convert the response body to JSON object.
-            return response.extract_json();
-          })
-          // Parse the user details.
-          .then([](json::value jsonObject) { return jsonObject.as_string(); });
-
-  try {
-    postJson.wait();
-    const std::string token = postJson.get();
-    return token;
-  }
-  catch(const std::exception& e) {
-    std::cout << "Failed to sign in" << e.what();
-  }
-  return "";
+      // Parse the user details.
+      .then([](json::value jsonObject) { return jsonObject.as_string(); });
 }
 
-bool ds::ds_auth_service_t::signOut(const std::string& token)
+std::string ds::ds_auth_service_t::signInSync(const std::string& email,
+                                          const std::string& password)
+{
+  auto postJson = this->signIn(email, password);
+  try {
+    postJson.wait();
+    return postJson.get();
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return "";
+  }
+}
+
+pplx::task<bool> ds::ds_auth_service_t::signOut(const std::string& token)
 {
   const std::string url = this->url;
-  auto postJson =
-      pplx::create_task([url, token]() {
-        http_client client(U(url + "/logout"));
-        http_request request(methods::POST);
-        request.headers().add(U("Content-Type"), U("application/json"));
-        request.headers().add(U("Authorization"), U("Bearer " + token));
-        return client.request(request);
-      }).then([](http_response response) {
+  return pplx::create_task([url, token]() {
+           http_client client(U(url + "/logout"));
+           http_request request(methods::POST);
+           request.headers().add(U("Content-Type"), U("application/json"));
+           request.headers().add(U("Authorization"), U("Bearer " + token));
+           return client.request(request);
+         })
+      .then([](http_response response) {
         // Check the status code.
         if(response.status_code() != 200) {
           return false;
@@ -93,7 +109,17 @@ bool ds::ds_auth_service_t::signOut(const std::string& token)
         // Convert the response body to JSON object.
         return true;
       });
+}
 
-  postJson.wait();
-  return postJson.get();
+bool ds::ds_auth_service_t::signOutSync(const std::string& token)
+{
+  auto postJson = this->signOut(token);
+  try {
+    postJson.wait();
+    return postJson.get();
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return false;
+  }
 }
--- src/ds_auth_service.h
+++ src/ds_auth_service.h
@@ -5,13 +5,12 @@
 #ifndef DS_AUTH_SERVICE_H
 #define DS_AUTH_SERVICE_H
 
+#include <pplx/pplxtasks.h>
 #include <string>
 
 namespace ds {
   class ds_auth_service_t {
   public:
-    bool verifyToken(const std::string& token);
-
     /**
      * Create a new auth service class by using the given url to the auth
      * service
@@ -20,19 +19,50 @@ namespace ds {
     explicit ds_auth_service_t(const std::string& authUrl);
 
     /**
+     * Verify the given token
+     * @param token
+     * @return task object, use wait() and get() on it to receive the bool value
+     */
+    pplx::task<bool> verifyToken(const std::string& token);
+
+    /**
+     * Verify the given token
+     * @param token
+     * @return true if token is valid, otherwise false
+     */
+    bool verifyTokenSync(const std::string& token);
+
+    /**
+     * Sign into the auth service with given email and password
+     * @param email
+     * @param password
+     * @return task object, use wait() and get() on it to receive the value
+     */
+    pplx::task<std::string> signIn(const std::string& email,
+                                   const std::string& password);
+
+    /**
      * Sign into the auth service with given email and password
      * @param email
      * @param password
      * @return valid token when successful, otherwise empty string
      */
-    std::string signIn(const std::string& email, const std::string& password);
+    std::string signInSync(const std::string& email,
+                           const std::string& password);
+
+    /**
+     * Sign out of the auth service and invalidate the given token
+     * @param token
+     * @return task object, use wait() and get() on it to receive the bool value
+     */
+    pplx::task<bool> signOut(const std::string& token);
 
     /**
      * Sign out of the auth service and invalidate the given token
      * @param token
      * @return true if successful, otherwise false
      */
-    bool signOut(const std::string& token);
+    bool signOutSync(const std::string& token);
 
   private:
     std::string url;
