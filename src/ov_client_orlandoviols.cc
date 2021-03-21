a87bf0d1963b094a591dcfff2d7835cb01b189a4
Giso Grimm <giso.grimm@vegri.net>
2021-03-21 21:38:46 +0100

    avoid channel id name clashes

--- src/ov_client_orlandoviols.cc
+++ src/ov_client_orlandoviols.cc
@@ -244,7 +244,8 @@ stage_device_t get_stage_dev(nlohmann::json& dev)
     if(channels.is_array())
       for(auto ch : channels) {
         device_channel_t devchannel;
-        devchannel.id = stagedev.label + "c" + std::to_string(chcnt++);
+        devchannel.id = stagedev.label + "_" + std::to_string(stagedev.id) +
+                        "_c" + std::to_string(chcnt++);
         devchannel.sourceport = my_js_value(ch, "sourceport", std::string(""));
         devchannel.gain = my_js_value(ch, "gain", 1.0);
         nlohmann::json chpos(ch["position"]);
