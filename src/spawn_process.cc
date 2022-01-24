d32a056fe0954e43efc3ebd5ff8ced6ee141680e
Giso Grimm <giso.grimm@vegri.net>
2022-01-24 08:39:30 +0100

    do not use shell for spawning processes

--- src/spawn_process.cc
+++ src/spawn_process.cc
@@ -18,37 +18,91 @@
  */
 
 #include "spawn_process.h"
-#include "errmsg.h"
-#include "string.h"
+#include "../tascar/libtascar/include/tscconfig.h"
 #include <signal.h>
+#include <unistd.h>
+#ifndef _WIN32
+#include <sys/wait.h>
+#endif
+
+pid_t system2(const std::string& command, bool noshell = true)
+{
+  pid_t pid = -1;
+#ifndef _WIN32 // Windows has no fork.
+  pid = fork();
+  DEBUG(pid);
+  if(pid < 0) {
+    return pid;
+  } else if(pid == 0) {
+    /// Close all other descriptors for the safety sake.
+    for(int i = 3; i < 4096; ++i)
+      ::close(i);
+    DEBUG(setsid());
+    DEBUG(pid);
+    int retv = 0;
+    if(noshell) {
+      DEBUG(pid);
+      std::vector<std::string> pars = TASCAR::str2vecstr(command);
+      char* vpars[pars.size() + 1];
+      for(size_t k = 0; k < pars.size(); ++k) {
+        vpars[k] = strdup(pars[k].c_str());
+      }
+      vpars[pars.size()] = NULL;
+      DEBUG(pid);
+      if(pars.size()) {
+        retv = execvp(pars[0].c_str(), vpars);
+      }
+      DEBUG(pid);
+      for(size_t k = 0; k < pars.size(); ++k) {
+        DEBUG(vpars[k]);
+        free(vpars[k]);
+      }
+      DEBUG(pid);
+    } else {
+      DEBUG(pid);
+      retv = execl("/bin/sh", "sh", "-c", command.c_str(), NULL);
+      DEBUG(pid);
+    }
+    DEBUG(retv);
+    _exit(1);
+  }
+#endif
+  return pid;
+}
 
 spawn_process_t::spawn_process_t(const std::string& command)
     : h_pipe(NULL), pid(0)
 {
+  DEBUG(command);
   if(!command.empty()) {
-    size_t len(command.size() + 100);
-    char ctmp[len];
-    memset(ctmp, 0, len);
-    snprintf(ctmp, len, "sh -c \"%s >/dev/null & echo \\$!\"", command.c_str());
-    h_pipe = popen(ctmp, "r");
-    if(!h_pipe)
-      throw ErrMsg("Unable to start pipe with command \"" + std::string(ctmp) +
-                   "\".");
-    if(fgets(ctmp, 1024, h_pipe) != NULL) {
-      pid = atoi(ctmp);
-      if(pid == 0)
-        throw ErrMsg("Invalid subprocess PID (0) when starting command \"" +
-                     command + "\".");
-    }
+    // size_t len(command.size() + 100);
+    // char ctmp[len];
+    // memset(ctmp, 0, len);
+    // snprintf(ctmp, len, "sh -c \"%s >/dev/null & echo \\$!\"",
+    // command.c_str()); h_pipe = popen(ctmp, "r"); if(!h_pipe)
+    //  throw ErrMsg("Unable to start pipe with command \"" + std::string(ctmp)
+    //  +
+    //               "\".");
+    // if(fgets(ctmp, 1024, h_pipe) != NULL) {
+    //  pid = atoi(ctmp);
+    //  if(pid == 0)
+    //    throw ErrMsg("Invalid subprocess PID (0) when starting command \"" +
+    //                 command + "\".");
+    //}
+    pid = system2(command, true);
   }
 }
 
 spawn_process_t::~spawn_process_t()
 {
-  if(pid != 0)
+  DEBUG(pid);
+  if(pid != 0) {
     kill(pid, SIGTERM);
+    waitpid(pid, NULL, 0);
+  }
   if(h_pipe)
     fclose(h_pipe);
+  DEBUG(pid);
 }
 
 /*
